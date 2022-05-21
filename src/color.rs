3f07148666c6bc4e13ac4ff6e8c781d4f3fc909e
Genevieve Alfirevic <gen@nyble.dev>
2022-05-21 00:02:48 -0500

    Add color constants

--- src/color.rs
+++ src/color.rs
@@ -11,15 +11,27 @@ pub struct Color {
 }
 
 impl Color {
-	pub fn rgb(r: f32, g: f32, b: f32) -> Self {
+	pub const RED: Color = Color::rgb(1.0, 0.0, 0.0);
+	pub const GREEN: Color = Color::rgb(0.0, 1.0, 0.0);
+	pub const BLUE: Color = Color::rgb(0.0, 0.0, 1.0);
+
+	pub const YELLOW: Color = Color::rgb(1.0, 1.0, 0.0);
+	pub const FUCHSIA: Color = Color::rgb(1.0, 0.0, 1.0);
+	pub const AQUA: Color = Color::rgb(0.0, 1.0, 1.0);
+
+	pub const TRANSPARENT: Color = Color::rgba(0.0, 0.0, 0.0, 0.0);
+	pub const BLACK: Color = Color::rgb(0.0, 0.0, 0.0);
+	pub const WHITE: Color = Color::rgb(1.0, 1.0, 1.0);
+
+	pub const fn rgb(r: f32, g: f32, b: f32) -> Self {
 		Self { r, g, b, a: 1.0 }
 	}
 
-	pub fn rgba(r: f32, g: f32, b: f32, a: f32) -> Self {
+	pub const fn rgba(r: f32, g: f32, b: f32, a: f32) -> Self {
 		Self { r, g, b, a }
 	}
 
-	pub fn grey(v: f32) -> Self {
+	pub const fn grey(v: f32) -> Self {
 		Self {
 			r: v,
 			g: v,
